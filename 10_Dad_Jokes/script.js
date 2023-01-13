98c3a9f3ac6f714bd73722261c348c31584ad78b
ratikantmohanta <ratikantmohanta@yahoo.com>
2023-01-13 21:09:33 +0530

    C10 Dad Jokes

---
+++ 10_Dad_Jokes/script.js
@@ -0,0 +1,35 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+        },
+    };
+
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+
+    jokeElement.innerHTML = data.joke;
+}
+
+// Without Async
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json',
+//         },
+//     };
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             joke.innerHTML = data.joke;
+//         })
+// }
