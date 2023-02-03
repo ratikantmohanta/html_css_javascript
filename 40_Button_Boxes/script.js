8ad37251878ba027f1990dfbe95a36561a7dd003
ratikantmohanta <ratikantmohanta@yahoo.com>
2023-02-03 23:02:40 +0530

    C40 Button Boxes

---
+++ 40_Button_Boxes/script.js
@@ -0,0 +1,16 @@
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+function createBoxes() {
+    for (let y = 0; y < 4; y++) {
+        for (let x = 0; x < 4; x++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundPosition = `${-x * 125}px ${-y * 125}px`;
+
+            boxesContainer.appendChild(box);
+        }
+    }
+};
+
+createBoxes();
